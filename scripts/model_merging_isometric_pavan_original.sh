0d751c40feaef310c67cb56a8abb5371f871a7a8
Brian Cruz <12848114+rhymeswithlion@users.noreply.github.com>
2023-11-14 13:26:18 -0800

    Add model_merging_isometric_pavan_original test

---
+++ scripts/model_merging_isometric_pavan_original.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Import common things
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $SCRIPTS_DIR/common.sh
+cd $SCRIPTS_DIR/../model_merging
+
+#######################
+# Run the isometric model merging as shown in ./model_merging/README.md
+
+EVAL_TASK=rte
+
+# Using PavanNeerudu/gpt2-finetuned-mnli doesn't work b/c of tokenizer issues
+RTE_MODEL=PavanNeerudu/gpt2-finetuned-rte
+MNLI_MODEL=PavanNeerudu/gpt2-finetuned-mnli
+
+# We need to fix the tokenizer issues locally to get this to work
+# RTE_MODEL=./gpt2-finetuned-rte
+# MNLI_MODEL=./gpt2-finetuned-mnli
+
+# Isometric merge.
+python3 ./scripts/merge_and_evaluate.py  \
+    --models=$RTE_MODEL,$MNLI_MODEL \
+    --glue_task=$EVAL_TASK \
+    --n_examples=100
