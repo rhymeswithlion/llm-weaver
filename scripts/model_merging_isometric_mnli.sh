6116e9b6a12f705a2a1f0c39037bb081f9deec4f
Brian Cruz <12848114+rhymeswithlion@users.noreply.github.com>
2023-11-25 15:22:36 -0800

    Add mnli test notebook

---
+++ scripts/model_merging_isometric_mnli.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Import common things
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $SCRIPTS_DIR/common.sh
+cd $SCRIPTS_DIR/../model_merging
+
+#######################
+# Run the isometric model merging as shown in ./model_merging/README.md
+
+EVAL_TASK=mnli
+RTE_MODEL=textattack/roberta-base-MNLI
+MNLI_MODEL=textattack/roberta-base-MNLI
+# Isometric merge.
+python3 ./scripts/merge_and_evaluate.py  \
+    --models=$RTE_MODEL,$MNLI_MODEL \
+    --glue_task=$EVAL_TASK \
+    --n_examples=1000
+
